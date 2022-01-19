e8c52cdfa5f151b0772dacbd7b29d00c96c3f6a1
Firemark <marpiechula@gmail.com>
2022-01-19 20:02:54 +0100

    add run.ts

---
+++ src/web/run.ts
@@ -0,0 +1,30 @@
+import { exec, Func } from "../exec";
+import { createSvg } from "../svg";
+
+export function run(code: Func[]) {
+    const container = document.getElementById("image");
+    container.innerHTML = "";
+
+    const argument = 100.0;
+    const maxIteration = 16;
+    const cursor = exec(argument, maxIteration, code);
+    cursor.addMargin(20);
+    const serializer = new XMLSerializer();
+    const svg = createSvg(document, cursor);
+
+    container.appendChild(svg);
+}
+
+export function scrapeCode(): Func[] {
+    const codeNode = document.getElementById("code");
+    const code: Func[] = [];
+    codeNode.querySelectorAll(".function").forEach(funcNode => {
+        const name = (funcNode.querySelector(".name") as HTMLElement).innerText;
+        const tokens: string[] = [];
+        funcNode.querySelectorAll(".token").forEach(tokenNode => {
+            tokens.push((tokenNode as HTMLElement).dataset.token);
+        });
+        code.push({name, tokens});
+    });
+    return code;
+}
