a3fc74c716da8d7ac13c65fe0e1ac6be5d6b2180
Firemark <marpiechula@gmail.com>
2022-12-30 17:30:41 +0100

    Add DragContext to allow support touch and mouse event, add/remove over class for valid classes on drap&drop

--- src/web/views/token.ts
+++ src/web/views/token.ts
@@ -34,56 +34,98 @@ export class TokensView extends View {
     }
 
     private setTokenEvents(node: HTMLElement) {
-        const { createElement } = this;
         node.addEventListener('mousedown', dragStart, false);
 
         function dragStart(event: MouseEvent) {
             event.preventDefault();
-            const dragNode = createDragNode();
-            document.body.appendChild(dragNode);
-            setCoords(dragNode, getCoords(dragNode, event));
-            document.addEventListener('mousemove', dragMove(dragNode), false);
-            document.addEventListener('mouseup', dragStop(dragNode), false);
+            const context = new DragContext(node, getMouseCoords);
+            context.init();
+            context.update(event);
+
+            document.addEventListener('mousemove', dragMove(context), false);
+            document.addEventListener('mouseup', dragStop(context), false);
         }
 
-        function createDragNode(): HTMLElement {
-            const dragNode = createElement({
-                name: 'span',
-                classes: ['fract-token', 'move'],
-            });
-            dragNode.dataset.token = node.dataset.token;
-            dragNode.dataset.type = node.dataset.type;
-            dragNode.style.backgroundImage = node.style.backgroundImage;
-            dragNode.dataset.tokenId = node.id;
-            return dragNode;
-        }
-
-        function dragMove(dragNode: HTMLElement) {
-            return (event: MouseEvent) => {
+        function dragMove<EventType extends Event>(context: DragContext<EventType>) {
+            return (event: EventType) => {
                 event.preventDefault();
-                const [x, y] = getCoords(dragNode, event);
-                const nodes = document.elementsFromPoint(x, y);
-                console.log(nodes);
-                setCoords(dragNode, [x, y]);
+                context.update(event);
             }
         }
 
-        function dragStop(dragNode: HTMLElement) {
-            return (event: MouseEvent) => {
+        function dragStop<EventType extends Event>(context: DragContext<EventType>) {
+            return (event: EventType) => {
                 event.preventDefault();
-                dragNode.remove();
+                context.clear();
             }
         }
 
-        function getCoords(dragNode: HTMLElement, event: MouseEvent): [number, number] {
-            const x = event.clientX - dragNode.clientWidth / 2;
-            const y = event.clientY - dragNode.clientHeight / 2;
-            return [x, y];
-        }
-
-        function setCoords(dragNode: HTMLElement, [x, y]: [number, number]) {
-            dragNode.style.left = `${x.toFixed()}px`;
-            dragNode.style.top = `${y.toFixed()}px`;
+        function getMouseCoords(event: MouseEvent): [number, number] {
+            return [event.clientX, event.clientY];
         }
     }
 }
+
+
+class DragContext<EventType extends Event> {
+    public dragNode: HTMLElement;
+    public overNode: HTMLElement | null;
+    private _getCoords: (event: EventType) => [number, number];
+    private VALID_CLASSES = ['fract-token-span', 'fract-token', 'fract-token-group'];
+
+    constructor(parentNode: HTMLElement, getCoords: (event: EventType) => [number, number]) {
+        this.dragNode = this.createDragNode(parentNode);
+        this.overNode = null;
+        this._getCoords = getCoords;
+    }
+
+    public init() {
+        document.body.appendChild(this.dragNode);
+    }
+    
+    public clear() {
+        this.dragNode.remove();
+    }
+
+    public update(event: EventType) {
+        const [x, y] = this.getCoords(event);
+        this.updateCoords(x, y);
+        if (this.overNode !== null) {
+            this.overNode.classList.remove("over");
+        }
+        this.updateOverNode(x, y);
+        if (this.overNode !== null) {
+            this.overNode.classList.add("over");
+        }
+    }
+
+    private updateCoords(x: number, y: number) {
+        this.dragNode.style.left = `${x.toFixed()}px`;
+        this.dragNode.style.top = `${y.toFixed()}px`;
+    }
+
+    private updateOverNode(x: number, y: number) {
+        const nodes = document.elementsFromPoint(x, y);
+        const node = nodes.find(
+            node => !node.classList.contains("move") && this.VALID_CLASSES.some(c => node.classList.contains(c))
+        );
+        this.overNode = (node as HTMLElement) || null;
+    }
+
+    private getCoords(event: EventType): [number, number] {
+        const [x, y] = this._getCoords(event);
+        const nx = x - this.dragNode.clientWidth / 2;
+        const ny = y - this.dragNode.clientHeight / 2;
+        return [nx, ny];
+    }
+
+    private createDragNode(node: HTMLElement): HTMLElement {
+        const dragNode = document.createElement('span');
+        dragNode.classList.add('fract-token', 'move');
+        dragNode.dataset.token = node.dataset.token;
+        dragNode.dataset.type = node.dataset.type;
+        dragNode.style.backgroundImage = node.style.backgroundImage;
+        dragNode.dataset.tokenId = node.id;
+        return dragNode;
+    }
+};
