01070ea91cdb87b34caf5a13278615415f5493df
Firemark <marpiechula@gmail.com>
2023-01-24 22:07:47 +0100

    Support tokenInfo with label

--- src/web/views/chooseDialog.ts
+++ src/web/views/chooseDialog.ts
@@ -1,5 +1,6 @@
 import { View } from "./view";
 import { TOKEN_FAMILIES, NAME_TO_FAMILY } from "@/web/tokensFamily";
+import { NAME_TO_TOKEN } from "@/web/tokens"
 import { DEFAULT_ICON_URL } from "@/web/consts";
 
 export class ChooseTokenDialogView extends View {
@@ -25,15 +26,32 @@ export class ChooseTokenDialogView extends View {
             this.node.textContent = 'No family :(';
         } 
         family.forEach(token => {
-            const spanNode = this.createElement({
+            const tokenInfo = NAME_TO_TOKEN.get(token);
+            if (tokenInfo === undefined) {
+                return;
+            }
+            const tokenWithLabelNode = this.createElement({
+                name: 'div',
+                classes: ['dialog-token'],
+            });
+            const tokenNode = this.createElement({
                 name: 'div',
                 classes: ['fract-token'],
+            });
+            tokenNode.dataset.token = token;
+            tokenNode.style.backgroundImage = `url(${this.#iconUrl}/${token}.svg)`;
+            tokenNode.style.width = `48px`;
+            tokenNode.style.height = `48px`;
+
+            const labelNode = this.createElement({
+                name: 'span',
+                classes: ['label'],
+                text: tokenInfo.label,
             })
-            spanNode.dataset.token = token;
-            spanNode.style.backgroundImage = `url(${this.#iconUrl}/${token}.svg)`;
-            spanNode.style.width = `48px`;
-            spanNode.style.height = `48px`;
-            this.node.appendChild(spanNode);
+
+            tokenWithLabelNode.appendChild(tokenNode);
+            tokenWithLabelNode.appendChild(labelNode);
+            this.node.appendChild(tokenWithLabelNode);
         });
         (this.node as HTMLDialogElement).showModal();
     }
